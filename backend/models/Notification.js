2047c390135f5c42885aaeaa8b44f1f2b11f20be
RejaulBSSE1324 <121537312+RejaulBSSE1324@users.noreply.github.com>
2024-11-05 11:38:39 +0600

    Delete backend directory

--- backend/models/Notification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const notificationSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Notification', notificationSchema);
--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { signIn, signUp } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/signup', signUp);  // SignUp route
-router.post('/signin', signIn);
-
-module.exports = router;
